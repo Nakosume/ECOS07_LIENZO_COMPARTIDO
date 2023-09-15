b633a6deb22493b100698e43caf3c3b78a3b4352
Sebastian Romero <nakosume@gmail.com>
2023-09-15 14:57:13 -0500

    Color Picker works

--- SERVER/public/client.js
+++ SERVER/public/client.js
@@ -4,7 +4,8 @@ let size = 20;
 let r = 0;
 let g = 0;
 let b = 0;
-let id = 0
+let col = 0;
+let id = 0;
 
 let elements = [];
 let cursors = []
@@ -16,21 +17,22 @@ function setup() {
   g = int(Math.random() * 255)
   b = int(Math.random() * 255)
   id = int(random() * 1000)
-  console.log("id: ", id)
+  //console.log("id: ", id)
 }
 
 function draw() {
   background(220);
-
+  noStroke();
+  
   //draw from elements list
   elements.forEach((element) => {
-    fill(color.color());
+    fill(element.col);
     ellipse(element.x, element.y, element.size, element.size);
   })
 
   //draw from cursors list
   cursors.forEach((element) => {
-    fill(color.color()),
+    fill(element.col),
       ellipse(element.x, element.y, element.size, element.size)
   })
 }
@@ -42,8 +44,11 @@ function mousePressed() {
     r: r,
     g: g,
     b: b,
+    col: color.value(),
     size
   }
+
+  console.log("color: ", color.value())
 
   socket.emit('send-element', element)
 }
@@ -55,9 +60,12 @@ function mouseDragged() {
     r: r,
     g: g,
     b: b,
+    col: color.value(),
     size,
     id: id
   }
+
+  console.log("color: ", color.value())
 
   socket.emit('send-cursor', element)
 }
@@ -68,7 +76,7 @@ socket.on('element-received',(element)=>{
 })
 
 socket.on('cursor-received',(element)=>{
-  console.log("element-received: ",element)
+  //console.log("element-received: ",element)
   let cursorIndex=cursors.findIndex((index)=>element.id==getItem.id)
   if(cursorIndex!=-1){
     cursors[cursorIndex]=element
